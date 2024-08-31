b7ddbdfeb37271b8d785add5243de928576110cc
Bogdawaa <bogdan6246@yandex.ru>
2024-08-31 22:15:34 +0300

    Подчистил комментарии

--- ToDoList/Application/AppDelegate.swift
+++ ToDoList/Application/AppDelegate.swift
@@ -29,28 +29,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
-            print(storeDescription.url?.path())
         })
         container.viewContext.shouldDeleteInaccessibleFaults = true
         container.viewContext.automaticallyMergesChangesFromParent = true
         return container
     }()
-
-//    // MARK: - Core Data Saving support
-//    func saveContext (with context: NSManagedObjectContext) {
-//        context.performAndWait {
-//            if context.hasChanges {
-//                do {
-//                    try context.save()
-//                } catch {
-//                    context.rollback()
-//                    let nserror = error as NSError
-//                    fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-//                }
-//            }
-//            context.rollback()
-//        }
-//    }
-
 }
 
